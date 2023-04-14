e131ac7b5accb037d1d9da711f029cfffad2b512
bahaasherif <bahaa_sherif00@yahoo.com>
2023-04-14 17:03:52 +0200

    Ex 2 added

---
+++ Unit 2/Assignment 1-C Basics/Ex 2.c
@@ -0,0 +1,18 @@
+/*
+ * Ex 2.c
+ *
+ *  Created on: Apr 14, 2023
+ *      Author: Bahaa
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int x;
+	printf("Enter an Integer: ");
+	fflush(stdout); fflush(stdin);
+	scanf("%d",&x);
+	printf("you entered: %d", x);
+}
