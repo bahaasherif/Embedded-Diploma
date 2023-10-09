2e771b2ce8bd17e6f6db3fbb9327621ec9bb52cf
bahaasherif <bahaa_sherif00@yahoo.com>
2023-10-09 05:37:06 +0300

    unit 3 lesson 2 added

---
+++ Unit 3/Lesson 2/app.c
@@ -0,0 +1,8 @@
+#include "uart.h"
+
+unsigned char string_buffer[100] = "learn in depth: bahaa sherif";
+unsigned char const string_buffer2[100] = "learn in depth: bahaa sherif";
+void main(void)
+{
+	Uart_Send_String(string_buffer);
+}
